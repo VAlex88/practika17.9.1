61a74b38348013e923b96a25d22a5f4e5777857a
VAlex88 <124186502+VAlex88@users.noreply.github.com>
2023-05-03 23:10:45 +0300

    practika17.9.1
    practika17.9.1

---
+++ praktika17.9.1.py
@@ -0,0 +1,31 @@
+def Bubble(array):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(array)):
+            for j in range(len(array) - i - 1):
+                if array[j] > array[j + 1]:
+                    array[j], array[j + 1] = array[j + 1], array[j]
+
+
+def BinarySearch(array, element):
+    if element < array[0] or element > array[-1]:
+        return False
+    first = 0
+    last = len(array)
+    while first < last - 1:
+        mid = (first + last) // 2
+        if element > array[mid]:
+            first = mid
+        else:
+            last = mid
+    return first
+
+
+L = [int(x) for x in input("Введите числа в любом порядке, через пробел: ").split()]
+element = int(input("Введите любое целое число: "))
+array = list(map(int, L))
+print(array)
+Bubble(array)
+print(array)
+print(BinarySearch(array, element))
